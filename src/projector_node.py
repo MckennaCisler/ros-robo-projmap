95a12b117bf5e755945ee6bc459092acaff80883
Henry Stone <henry_stone1@brown.edu>
2019-04-10 21:32:04 -0400

    depth data decoding

--- src/projector_node.py
+++ src/projector_node.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String, Float32MultiArray
 from sensor_msgs.msg import Image
 import numpy as np
 
@@ -12,23 +12,14 @@ class ProjectorNode:
     def __init__(self):
         self.sub_image = rospy.Subscriber("/image", Image, self.rgb_frame_cb)
         self.sub_depth = rospy.Subscriber("/depth", Image, self.depth_frame_cb)
-        sleep(0.5)
-        self.pub_img = rospy.Publisher("/image", Image, queue_size=5)
-        sleep(0.5)
-        bridge = CvBridge()
-        print('a')
-        self.pub_img.publish(
-            bridge.cv2_to_imgmsg(
-                np.random.randint(255, size=[6, 8, 3], dtype=np.uint8),
-                'rgb8'))
 
     def rgb_frame_cb(self, msg):
         assert msg.encoding == 'rgb8'
         img = np.frombuffer(msg.data, dtype=np.uint8).reshape([msg.height, msg.width, 3])
-        print(img)
 
     def depth_frame_cb(self, msg):
-        pass
+        assert msg.encoding == '16UC1'
+        depth = np.frombuffer(msg.data, dtype=np.uint16).reshape([msg.height, msg.width])
 
 if __name__ == '__main__':
     rospy.init_node('projector', anonymous=False)
